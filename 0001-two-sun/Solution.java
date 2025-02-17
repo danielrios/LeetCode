3b42aa8a62bf5b78c7e1572251295ff3dee8716e
danielrios <danielriosdev@gmail.com>
2025-02-16 22:56:55 -0300

    feat: Add solution to problem No.0001

--- 0001-two-sun/Solution.java
+++ 0001-two-sun/Solution.java
@@ -5,7 +5,7 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             int sum = target - nums[i];
             if (map.containsKey(sum)) {
-                return new int[]{i, map.get(target - nums[i])};
+                return new int[]{i, map.get(sum)};
             }
             map.put(nums[i], i);
         }
