00f01ef3f0a83ac84ec5a9ccf9f925ed1f665500
Daniel <49802471+danielrios@users.noreply.github.com>
2026-04-02 21:31:01 -0300

    refactor: Change to a more optimized approach to problem No.0020

--- 0020-valid-parentheses/Solution.java
+++ 0020-valid-parentheses/Solution.java
@@ -1,17 +1,19 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> myStack = new Stack<>();
-        for (char c : s.toCharArray()) {
-            if (c == '{' || c == '(' || c == '[') {
-                myStack.push(c);
-            } else if (!myStack.isEmpty() && (c == '}' && myStack.peek() == '{'
-                    || c == ')' && myStack.peek() == '('
-                    || c == ']' && myStack.peek() == '[')) {
-                myStack.pop();
-            } else {
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for(char ch:s.toCharArray()) {
+            if(ch == '(') {
+                stack.push(')');    
+            } else if(ch == '{') {
+                stack.push('}');    
+            } else if(ch == '[') {
+                stack.push(']');    
+            } else if(stack.isEmpty()|| stack.pop() != ch) { 
                 return false;
-            }
+            } 
         }
-        return myStack.isEmpty();
+        
+        return stack.isEmpty();
     }
 }
