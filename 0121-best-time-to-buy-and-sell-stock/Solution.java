55e3c7771aa6b541244dacdca0c8cd148e5945c1
Daniel <49802471+danielrios@users.noreply.github.com>
2026-04-14 22:12:51 -0300

    Refactor maxProfit method for better clarity

--- 0121-best-time-to-buy-and-sell-stock/Solution.java
+++ 0121-best-time-to-buy-and-sell-stock/Solution.java
@@ -1,16 +1,12 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int buyDay = prices[0];
-        int result = 0;
-
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] < buyDay) {
-                buyDay = prices[i];
-            } else {
-                result = Math.max(prices[i] - buyDay, result);
-            }
+        int buy = prices[0], maxProfit = 0;
+        
+        for(int i = 1; i < prices.length; i++) {
+            maxProfit = Math.max(prices[i] - buy, maxProfit);
+            buy = Math.min(buy, prices[i]);
         }
 
-        return result;
+        return maxProfit;
     }
 }
